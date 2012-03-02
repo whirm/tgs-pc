fd53f7ccf83349e0c33d283ec870722eb3984265
Elric Milon <whirm@gmx.com>
2012-03-02 20:13:15 +0100

    Automatically compile all .ui files found in src/ui

--- src/build_resources.sh
+++ src/build_resources.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
+set -e
+
 cd ui
-pyuic4 main.ui -o main.py
 pyrcc4 resources.qrc -o resources_rc.py
 
+for UI in *.ui ; do
+    echo "Compiling $UI..."
+    UIPY=$(echo $UI| sed 's/ui$/py/')
+    pyuic4 $UI -o $UIPY
+done
+echo "All done!"
