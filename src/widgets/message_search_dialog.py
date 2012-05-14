fd11181e93494c3110fad6f0b382ca0be1844aef
Elric Milon <whirm@gmx.com>
2012-05-14 20:33:39 +0200

    Message search dialog

--- src/widgets/message_search_dialog.py
+++ src/widgets/message_search_dialog.py
@@ -16,18 +16,30 @@ from widgets import ChatMessageWidget
 
 __all__=['MessageSearchDialog',]
 
+#Row name constants
+TEXT, MEMBER, SQUARE = range(3)
+
 class MessageSearchDialog(QtGui.QDialog, Ui_MessageSearchDialog):
     onSearchRequested = QtCore.pyqtSignal(unicode)
     def __init__(self, *argv, **kwargs):
         super(MessageSearchDialog, self).__init__(*argv, **kwargs)
         self.setupUi(self)
 
-        #Connect the joinSquare button to its callback
+        self._resetModel()        
+		#Connect the joinSquare button to its callback
         self.joinSquare_btn.clicked.connect(self.onJoinSquareClicked)
         self.followMessageAuthor_btn.clicked.connect(self.onJoinSquareClicked)
 
         self.search_line.returnPressed.connect(self.onSearchReady)
         self.search_btn.clicked.connect(self.onSearchReady)
+
+    def _resetModel(self):
+        #Set up the square list data model
+        self._model = QtGui.QStandardItemModel(0, 3, self.results_list)
+        self._model.setHeaderData(TEXT, QtCore.Qt.Horizontal, "Title")
+        self._model.setHeaderData(MEMBER, QtCore.Qt.Horizontal, "Member")
+        self._model.setHeaderData(SQUARE, QtCore.Qt.Horizontal, "Square")
+        self.results_list.setModel(self._model)
 
     def onJoinSquareClicked(self):
         #TODO
@@ -44,5 +56,20 @@ class MessageSearchDialog(QtGui.QDialog, Ui_MessageSearchDialog):
         msgBox.exec_()
 
     def onSearchReady(self):
-        self.onSearchRequested.emit(self.search_line.text())
+        if self.search_line.text():
+            self.onSearchRequested.emit(self.search_line.text())
+            self.search_btn.setDisabled(True)
+            self.search_line.setDisabled(True)
 
+    def addResult(self, text, member, square):
+        self._model.insertRow(0)
+        self._model.setData(self._model.index(0, TEXT), text)
+        self._model.setData(self._model.index(0, MEMBER), member)
+        self._model.setData(self._model.index(0, SQUARE), square)
+
+    def clearResultsList(self):
+        self._resetModel()
+
+    def onSearchFinished(self):
+        self.search_btn.setDisabled(False)
+        self.search_line.setDisabled(False)
